a0449c19cbc250ba0ddd94e1de5fd6ed9ee74026
skson0x6ab <skson0x6ab@gmail.com>
2024-06-27 13:16:10 +0900

    rename project

---
+++ README.md
@@ -0,0 +1,4 @@
+1. npm run dev -> main.js -> 
+2. 
+3. package.json: dependenices 및 script 관리 파일
+2. package-lock.json은 위의 추가 파일
---
+++ sksonDashboard/src/views/releasenote/FF14.vue
@@ -0,0 +1,38 @@
+<!-- src/views/releasenote/FF14.vue -->
+<template>
+  <div>
+    <CTable striped :columns="column" :items="groupList" />
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import FF14 from '@/views/releasenote/ff14.json';
+
+const groupList = FF14;
+
+export default {
+  data() {
+    return {
+      column: [
+        {
+          key: 'category',
+          label: 'Category',
+          _props: { scope: 'col' },
+        },
+        {
+          key: 'text',
+          label: 'Text',
+          _props: { scope: 'col' },
+        },
+        {
+          key: 'date',
+          label: 'Date',
+          _props: { scope: 'col' },
+        },
+      ],
+      groupList,
+    };
+  },
+};
+</script>
